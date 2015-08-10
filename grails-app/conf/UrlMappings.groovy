e04f96280f6cf5b213c2572407bd892fafa49b36
Karina Azevedo <kma@cin.ufpe.br>
2015-08-09 23:26:30 -0300

    Update UrlMappings.groovy

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,14 @@
 class UrlMappings {
 
 	static mappings = {
+		
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
+        "/"(controller:"Task")
         "500"(view:'/error')
 	}
 }
