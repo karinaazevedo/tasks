6a5faa4056833d4e9e2bfd5cf8e106e3be9872bf
Karina Azevedo <kma@cin.ufpe.br>
2015-08-09 23:32:22 -0300

    Update UrlMappings.groovy

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,11 @@
 class UrlMappings {
 
 	static mappings = {
+	"/task/listByProperty/$propertyName?(.$format)/$propertyValue?(.$format)"{
+            constraints{
+                //propertyName(matches: /^\w$/)
+            }
+        }
 		
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
