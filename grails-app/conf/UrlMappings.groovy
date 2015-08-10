4ca0c671c8c92a6e0c02b36bc85f6247737df38f
karinaazevedo2 <kari.pereira@yahoo.com.br>
2015-08-09 23:51:40 -0300

    "gitignore"

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,19 @@
 class UrlMappings {
 
 	static mappings = {
+	"/task/listByProperty/$propertyName?(.$format)/$propertyValue?(.$format)"{
+            constraints{
+                //propertyName(matches: /^\w$/)
+            }
+        }
+		
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
+        "/"(controller:"Task")
         "500"(view:'/error')
 	}
 }
